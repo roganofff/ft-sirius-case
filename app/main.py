d01229fac90f75b0da5bd19ea2d2756ac17a0eed
Rin2396 <kolkareva.dayana2016@gmail.com>
2025-04-18 20:00:39 +0300

    folder created

--- app/main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from app.database import Base, engine
-from app.api.routes_user import router as user_router
-
-Base.metadata.create_all(bind=engine)
-
-app = FastAPI()
-app.include_router(user_router)
