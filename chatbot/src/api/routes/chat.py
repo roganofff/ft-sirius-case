6c40e660181cdc06f890f6f05f789f9ed120997c
roganofff <matematic1337@gmail.com>
2025-04-19 07:13:17 +0300

    feat: updated chat.py

--- chatbot/src/api/routes/chat.py
+++ chatbot/src/api/routes/chat.py
@@ -6,10 +6,10 @@ router = APIRouter()
 
 templates = Jinja2Templates(directory='chatbot/src/templates')
 
-@router.get("/", response_class=HTMLResponse)
-async def welcome(request: Request):
-    return templates.TemplateResponse('index.html', {'request': request})
+# @router.get("/", response_class=HTMLResponse)
+# async def welcome(request: Request):
+#     return templates.TemplateResponse('index.html', {'request': request})
 
 @router.get("/chat", response_class=HTMLResponse)
 async def chat(request: Request):
-    return templates.TemplateResponse('chat.html', {'request': request})
+    return templates.TemplateResponse('index.html', {'request': request})
